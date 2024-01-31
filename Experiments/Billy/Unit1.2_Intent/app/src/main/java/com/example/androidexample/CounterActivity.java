f702a5efabe8019708e79d9c830bda6c30245b70
Billy <bpdang07@iastate.edu>
2024-01-31 10:00:40 -0600

    Test files my Experiemnts Folder now

---
+++ Experiments/Billy/Unit1.2_Intent/app/src/main/java/com/example/androidexample/CounterActivity.java
@@ -0,0 +1,58 @@
+package com.example.androidexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CounterActivity extends AppCompatActivity {
+
+    private TextView numberTxt; // define number textview variable
+    private Button increaseBtn; // define increase button variable
+    private Button decreaseBtn; // define decrease button variable
+    private Button backBtn;     // define back button variable
+
+    private int counter = 0;    // counter variable
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_counter);
+
+        /* initialize UI elements */
+        numberTxt = findViewById(R.id.number);
+        increaseBtn = findViewById(R.id.counter_increase_btn);
+        decreaseBtn = findViewById(R.id.counter_decrease_btn);
+        backBtn = findViewById(R.id.counter_back_btn);
+
+        /* when increase btn is pressed, counter++, reset number textview */
+        increaseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberTxt.setText(String.valueOf(++counter));
+            }
+        });
+
+        /* when decrease btn is pressed, counter--, reset number textview */
+        decreaseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberTxt.setText(String.valueOf(--counter));
+            }
+        });
+
+        /* when back btn is pressed, switch back to MainActivity */
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CounterActivity.this, MainActivity.class);
+                intent.putExtra("NUM", String.valueOf(counter));  // key-value to pass to the MainActivity
+                startActivity(intent);
+            }
+        });
+
+    }
+}
