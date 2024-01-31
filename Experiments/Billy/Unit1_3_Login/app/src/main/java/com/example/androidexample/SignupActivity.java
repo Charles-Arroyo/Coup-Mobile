cf58cb013593b28f1494dad2dc500664f3ac26e4
Billy <bpdang07@iastate.edu>
2024-01-31 10:06:27 -0600

    New Test Files

---
+++ Experiments/Billy/Unit1_3_Login/app/src/main/java/com/example/androidexample/SignupActivity.java
@@ -0,0 +1,62 @@
+package com.example.androidexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SignupActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;  // define username edittext variable
+    private EditText passwordEditText;  // define password edittext variable
+    private EditText confirmEditText;   // define confirm edittext variable
+    private Button loginButton;         // define login button variable
+    private Button signupButton;        // define signup button variable
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        /* initialize UI elements */
+        usernameEditText = findViewById(R.id.signup_username_edt);  // link to username edtext in the Signup activity XML
+        passwordEditText = findViewById(R.id.signup_password_edt);  // link to password edtext in the Signup activity XML
+        confirmEditText = findViewById(R.id.signup_confirm_edt);    // link to confirm edtext in the Signup activity XML
+        loginButton = findViewById(R.id.signup_login_btn);    // link to login button in the Signup activity XML
+        signupButton = findViewById(R.id.signup_signup_btn);  // link to signup button in the Signup activity XML
+
+        /* click listener on login button pressed */
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* when login button is pressed, use intent to switch to Login Activity */
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);  // go to LoginActivity
+            }
+        });
+
+        /* click listener on signup button pressed */
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /* grab strings from user inputs */
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                String confirm = confirmEditText.getText().toString();
+
+                if (password.equals(confirm)){
+                    Toast.makeText(getApplicationContext(), "Signing up", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Password don't match", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+}
