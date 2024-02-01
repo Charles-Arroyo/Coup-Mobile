f9555295f0f23d4bdb2bf60a20e917fb3d3d0aa1
bhoo <bhoo@iastate.edu>
2024-02-01 15:24:16 -0600

    set up the spring boot

--- Experiments/Bo/helloworld/src/main/java/coms309/Application.java
+++ Experiments/Bo/helloworld/src/main/java/coms309/Application.java
@@ -14,6 +14,7 @@ public class Application {
 	
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
+        System.out.println("fds");
     }
 
 }
