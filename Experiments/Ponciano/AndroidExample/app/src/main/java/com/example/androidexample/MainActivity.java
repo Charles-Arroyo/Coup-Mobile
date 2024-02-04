1a036a987fa9100a4b90aa0e11808ef05ee321ff
Ponciano Ramirez <pram1347@iastate.edu>
2024-02-04 01:16:08 -0600

    Applied stash and resolved conflicts

---
+++ Experiments/Ponciano/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.androidexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView messageText;   // define message textview variable
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);             // link to Main activity XML
+
+        /* initialize UI elements */
+        messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
+        messageText.setText("Hello World");
+    }
+}
