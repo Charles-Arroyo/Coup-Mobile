03425603e9bd59068efe9e37154cffa5a6187e01
Charles <Cfarroyo@iastate.edu>
2024-02-19 23:56:54 -0600

    Creating applicaiton

---
+++ Experiments/CharlesArroyo/Unit 2/Example1/springboot_example/src/main/java/onetoone/Application.java
@@ -0,0 +1,19 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Sample Spring Boot Application.
+ *
+ * @author Vivek Bengre
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
+
