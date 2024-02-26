d59f47d0c6d62457b4ef3f15e4064a8932dd2a19
Bo <bhoo@iastate.edu>
2024-02-26 16:57:50 -0600

    changed folders

--- Experiments/Bo/unit2/unit2_2/springboot_example/src/main/java/onetomany/Users/User.java
+++ Experiments/Bo/unit2/unit2_2/springboot_example/src/main/java/onetomany/Users/User.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 
 import onetomany.Laptops.Laptop;
 import onetomany.Phones.Phone;
