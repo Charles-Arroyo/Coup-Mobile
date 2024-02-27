98ea86a2d51c21be9765ee271ab307eced8f18ad
Bo <bhoo@iastate.edu>
2024-02-26 23:19:36 -0600

    got rid of the unit for fresh repo clone

--- Experiments/Bo/unit2/unit2_2/springboot_example/src/main/java/onetomany/Phones/PhoneRepository.java
+++
@@ -1,7 +0,0 @@
-package onetomany.Phones;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhoneRepository extends JpaRepository<Phone, Long> {
-    Phone findById(int id);
-}
