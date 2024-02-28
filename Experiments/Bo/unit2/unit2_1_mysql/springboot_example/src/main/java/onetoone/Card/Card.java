b37852766825712512d7e82241cf9277374a9a6e
Bo <bhoo@iastate.edu>
2024-02-27 22:48:36 -0600

    created a Entity/class

--- Experiments/Bo/unit2/unit2_1_mysql/springboot_example/src/main/java/onetoone/Card/Card.java
+++ Experiments/Bo/unit2/unit2_1_mysql/springboot_example/src/main/java/onetoone/Card/Card.java
@@ -1,4 +1,20 @@
 package onetoone.Card;
 
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+import onetoone.Users.User;
+import onetoone.Laptops.Laptop;
+
+
+@Entity
 public class Card {
+
+
 }
