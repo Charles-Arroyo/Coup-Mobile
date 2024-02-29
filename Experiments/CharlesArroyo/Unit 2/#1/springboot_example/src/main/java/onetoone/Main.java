8c3f07d0650ab503b2d409cd59f24032feef06f4
Charles <Cfarroyo@iastate.edu>
2024-02-28 18:39:08 -0600

    adding files

--- Experiments/CharlesArroyo/Unit 2/#1/springboot_example/src/main/java/onetoone/Main.java
+++ Experiments/CharlesArroyo/Unit 2/#1/springboot_example/src/main/java/onetoone/Main.java
@@ -7,11 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import onetoone.Laptops.Laptop;
-import onetoone.Laptops.LaptopRepository;
-import onetoone.Users.User;
-import onetoone.Users.UserRepository;
 import onetoone.Accounts.Account;
 import onetoone.Accounts.AccountRepository;
 
