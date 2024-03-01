2433a3efb10ee0ecd7f222a4d405c4796b20c235
Ponciano <pram1347@iastate.edu>
2024-03-01 01:27:13 -0600

    added login/register menu

---
+++ Frontend/app/src/main/java/com/example/coupv2/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.coupv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the content view to the layout resource file for LoginActivity
+        // Replace 'activity_login' with the actual name of your layout file if different
+        setContentView(R.layout.activity_login);
+    }
+}
--- Frontend/app/src/main/java/com/example/coupv2/MainActivity.java
+++ Frontend/app/src/main/java/com/example/coupv2/MainActivity.java
@@ -7,8 +7,10 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    //take in last saved state
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //build layout off xml file
         setContentView(R.layout.activity_main);
     }
 }
