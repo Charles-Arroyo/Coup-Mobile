bd8123a781cfe1327191f8e9335c92e4c2e8c769
bpdang07 <158317097+bpdang07@users.noreply.github.com>
2024-03-02 23:26:52 -0600

    reg

--- Frontend/app/src/main/java/com/example/coupv2/SignupActivity.java
+++ Frontend/app/src/main/java/com/example/coupv2/SignupActivity.java
@@ -1,100 +1,114 @@
 package com.example.coupv2;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.coupv2.app.AppController;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SignupActivity extends AppCompatActivity {
 
-    private EditText usernameEditText;
+    private EditText emailIdEditText;
     private EditText passwordEditText;
-    private EditText confirmEditText;
     private Button loginButton;
     private Button signupButton;
 
-    private static final String URL_SIGNUP = "https://example.com/signup"; // Adjust the URL_SIGNUP according to your server's signup endpoint
-//    private static final String URL_JSON_OBJECT = "https://coms-309-023.class.las.iastate.edu";
+    private static final String URL_SIGNUP = "http://10.90.73.176:8080/signup";
+//    private static final String URL_SIGNUP = "http://coms-309-023.class.las.iastate.edu:8080/signup";
     // success
-    private static final String URL_JSON_OBJECT = "https://a9d64c4f-e136-411d-9914-ca9fdc127577.mock.pstmn.io";
+//    private static final String URL_SIGNUP = "https://c0552578-2f02-4669-999e-030ad7fa3950.mock.pstmn.io";
     //fail
-//    private static final String URL_JSON_OBJECT = "https://e240d7cb-1f58-4450-aafc-17819ecd7566.mock.pstmn.io";
+//    private static final String URL_SIGNUP = "https://fc027c91-5b3c-49e3-8239-a0223c763b2a.mock.pstmn.io";
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_signup);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        emailIdEditText = findViewById(R.id.signup_email_edt);
+        passwordEditText = findViewById(R.id.signup_password_edt);
+        loginButton = findViewById(R.id.signup_login_btn);
+        signupButton = findViewById(R.id.signup_signup_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailIdEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                if (!email.isEmpty() && !password.isEmpty()) {
+                    performSignup(email, password);
+                } else {
+                    Toast.makeText(SignupActivity.this, "Please enter both email and password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void performSignup(String emailId, String password) {
+        JSONObject jsonRequest = new JSONObject();
+        try {
+            jsonRequest.put("emailId", emailId);
+            jsonRequest.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        RequestQueue requestQueue = AppController.getInstance().getRequestQueue();
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, URL_SIGNUP, jsonRequest,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+
+//                            String message = response.getString("message");
 //
-//        usernameEditText = findViewById(R.id.signup_username_edt);
-//        passwordEditText = findViewById(R.id.signup_password_edt);
-//        confirmEditText = findViewById(R.id.signup_confirm_edt);
-//        loginButton = findViewById(R.id.signup_login_btn);
-//        signupButton = findViewById(R.id.signup_signup_btn);
-//
-//        loginButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//
-//        signupButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                String username = usernameEditText.getText().toString();
-//                String password = passwordEditText.getText().toString();
-//                String confirm = confirmEditText.getText().toString();
-//
-//                if (!username.isEmpty() && !password.isEmpty() && !confirm.isEmpty()) {
-//                    if (password.equals(confirm)) {
-//                        performSignup(username, password);
-//                    } else {
-//                        Toast.makeText(SignupActivity.this, "Password does not match confirmation", Toast.LENGTH_SHORT).show();
-//                    }
-//                } else {
-//                    Toast.makeText(SignupActivity.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//        });
-//    }
-//
-//    private void performSignup(String username, String password) {
-//        JSONObject jsonRequest = new JSONObject();
-//        try {
-//            jsonRequest.put("username", username);
-//            jsonRequest.put("password", password);
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//        }
-//
-//        RequestQueue requestQueue = AppController.getInstance().getRequestQueue();
-////        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, URL_SIGNUP, jsonRequest,
-//                new Response.Listener<JSONObject>() {
-//                    @Override
-//                    public void onResponse(JSONObject response) {
-//                        try {
-//                            boolean success = response.getBoolean("success");
-//                            if (success) {
-//                                Toast.makeText(SignupActivity.this, "Signup successful!", Toast.LENGTH_SHORT).show();
-//                                Intent loginIntent = new Intent(SignupActivity.this, LoginActivity.class);
-//                                startActivity(loginIntent);
-//                            } else {
-//                                Toast.makeText(SignupActivity.this, response.getString("message"), Toast.LENGTH_SHORT).show();
-//                            }
-//                        } catch (JSONException e) {
-//                            e.printStackTrace();
-//                            Toast.makeText(SignupActivity.this, "Invalid response from server", Toast.LENGTH_SHORT).show();
-//                        }
-//                    }
-//                },
-//                new Response.ErrorListener() {
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                        Toast.makeText(SignupActivity.this, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
-//                    }
-//                });
-//
-//        requestQueue.add(jsonObjectRequest);
-//    }
+//                            if (message.equals("success"))
+
+                            boolean success = response.getBoolean("success");
+
+                            if (success) {
+                                // Successful signup
+                                Toast.makeText(SignupActivity.this, "Signup successful!", Toast.LENGTH_SHORT).show();
+                                Intent mainIntent = new Intent(SignupActivity.this, LoginActivity.class);
+                                startActivity(mainIntent);
+                            } else {
+                                // Failed signup
+                                Toast.makeText(SignupActivity.this, response.getString("message"), Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(SignupActivity.this, "Invalid response from server", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle errors here
+                        Toast.makeText(SignupActivity.this, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        requestQueue.add(jsonObjectRequest);
+    }
 }
