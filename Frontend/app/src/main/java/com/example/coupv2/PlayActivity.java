a92f2f3144f7a4d3d6cfcb2598c4d03bcedcd72a
Ponciano <pram1347@iastate.edu>
2024-03-03 19:35:24 -0600

    made changes to PlayActivity and its xml file

--- Frontend/app/src/main/java/com/example/coupv2/PlayActivity.java
+++ Frontend/app/src/main/java/com/example/coupv2/PlayActivity.java
@@ -5,19 +5,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PlayActivity extends AppCompatActivity {
-
+    private Button winButton;
+    private Button lossButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mitra);            // link to Login activity XML
+        setContentView(R.layout.activity_play);            // link to Login activity XML
+        //connect xml ids
+        winButton = findViewById(R.id.win_btn);
+        lossButton = findViewById(R.id.loss_btn);
+        winButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Grab strings from user inputs
 
+
+                // go back to main menu
+                Intent menuIntent = new Intent(PlayActivity.this, MenuActivity.class);
+                startActivity(menuIntent);
+
+            }
+        });
+        lossButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // go back to main menu
+                Intent menuIntent = new Intent(PlayActivity.this, MenuActivity.class);
+                startActivity(menuIntent);
+            }
+        });
     }
 }
