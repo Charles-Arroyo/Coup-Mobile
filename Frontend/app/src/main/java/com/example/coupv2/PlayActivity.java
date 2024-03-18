8d6ed0279cf2933bbb78c6fae566d421fcd693a4
Ponciano <pram1347@iastate.edu>
2024-03-17 23:35:43 -0500

    added icons to game screen

--- Frontend/app/src/main/java/com/example/coupv2/PlayActivity.java
+++ Frontend/app/src/main/java/com/example/coupv2/PlayActivity.java
@@ -1,12 +1,7 @@
 package com.example.coupv2;
 
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,7 +12,7 @@ public class PlayActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mitra);            // link to Login activity XML
+        setContentView(R.layout.activity_play);            // link to Login activity XML
 
     }
 }
