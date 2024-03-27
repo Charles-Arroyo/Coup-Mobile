59eb11a0c5a2640919468986a17658f63079cb3e
Bo Oo <bhoo@iastate.edu>
2024-03-27 14:37:29 -0500

    trying to doc

---
+++ Backend/gameFile/src/main/java/database/Docket/Docket.java
@@ -0,0 +1,4 @@
+package database.Docket;
+
+public class Docket {
+}
---
+++ Backend/gameFile/src/main/java/database/Docket/SpringFoxConfig.java
@@ -0,0 +1,16 @@
+package database.Docket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
