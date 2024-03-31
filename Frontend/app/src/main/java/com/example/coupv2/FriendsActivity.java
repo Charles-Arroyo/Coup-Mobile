191932f913cd3e3e4d97ba6a1d41e0fa0410ddee
bpdang07 <158317097+bpdang07@users.noreply.github.com>
2024-03-31 17:30:07 -0500

    more changes to UI for admin

--- Frontend/app/src/main/java/com/example/coupv2/FriendsActivity.java
+++ Frontend/app/src/main/java/com/example/coupv2/FriendsActivity.java
@@ -1,8 +1,6 @@
 package com.example.coupv2;
 
 import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.graphics.LightingColorFilter;
 import android.os.Bundle;
 import android.view.View;
@@ -173,7 +171,7 @@ public class FriendsActivity extends AppCompatActivity {
                         for (int i = 0; i < friendsArray.length(); i++) {
                             JSONObject friend = friendsArray.getJSONObject(i);
                             String email = friend.getString("friendEmail2"); // Use "friendEmail2" to get the email
-                            boolean isActive = friend.getBoolean("active"); // Get active status
+//                            boolean isActive = friend.getBoolean("active"); // Get active status
 
                             View friendView = getLayoutInflater().inflate(R.layout.friend_item, friendsLayout, false);
 
@@ -182,12 +180,12 @@ public class FriendsActivity extends AppCompatActivity {
                             emailButton.setOnClickListener(v -> showUserStats(email));
 
                             View activeButton = friendView.findViewById(R.id.active);
-                            activeButton.setBackgroundTintList(ColorStateList.valueOf(isActive ? Color.GREEN : Color.RED));
+//                            activeButton.setBackgroundTintList(ColorStateList.valueOf(isActive ? Color.GREEN : Color.RED));
 
                             // Set onClickListener for the activeButton to show a toast
                             activeButton.setOnClickListener(v -> {
-                                String statusMessage = isActive ? "Online" : "Offline";
-                                Toast.makeText(FriendsActivity.this, email + " is " + statusMessage, Toast.LENGTH_SHORT).show();
+//                                String statusMessage = isActive ? "Online" : "Offline";
+//                                Toast.makeText(FriendsActivity.this, email + " is " + statusMessage, Toast.LENGTH_SHORT).show();
                             });
 
 
