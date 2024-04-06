92140578a47d907dadca3a257d0250432a98e227
Ponciano Ramirez <pram1347@iastate.edu>
2024-04-06 02:22:28 -0500

    fixed afterCreateLobby

--- Frontend/app/src/main/java/com/example/coupv2/AfterCreateLobbyActivity.java
+++ Frontend/app/src/main/java/com/example/coupv2/AfterCreateLobbyActivity.java
@@ -73,18 +73,26 @@ public class AfterCreateLobbyActivity extends AppCompatActivity implements WebSo
     @Override
     public void onWebSocketMessage(String fullMessage) {
         runOnUiThread(() -> {
-            int i = fullMessage.indexOf(":");
-            if (i != -1) {
-                String username = fullMessage.substring(0, i).trim();
-                String message = fullMessage.substring(i + 1).trim();
+            Log.d("WebSocketMessage", fullMessage);
+            if ("Lobby is full".equals(fullMessage)) {
+                Log.d("WebSocketMessage", "ffsffs");
+                 isLobbyFull = true;
+                goToNewActivity(); // Call method to transition to the new activity
+            }
+            else{
+                int i = fullMessage.indexOf(":");
+                if (i != -1) {
+                    String username = fullMessage.substring(0, i).trim();
+                    String message = fullMessage.substring(i + 1).trim();
 
-                // Log the username and message using Log.d (debug level)
-                Log.d("WebSocketMessage", "User: " + username + " Message: " + message);
+                        // Log the username and message using Log.d (debug level)
+                        Log.d("WebSocketMessage", "User: " + username + " Message: " + message);
 
-                addMessageToLayout(username, message);
-            } else {
-                // Log a warning if the message format is not as expected
-                Log.w("WebSocketMessage", "Message does not contain a colon (:) separator: " + fullMessage);
+                        addMessageToLayout(username, message);
+                } else {
+                    // Log a warning if the message format is not as expected
+                    Log.w("WebSocketMessage", "Message does not contain a colon (:) separator: " + fullMessage);
+                }
             }
         });
     }
